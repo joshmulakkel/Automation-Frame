3368c02e87e0e13d3485af65fdad59b52f7fc570
Joshy Varghese <joshyvarghese@Joshys-MacBook-Pro.local>
2025-11-11 12:05:06 +0530

    Initial commit

--- src/test/java/com/utility/BrowserUtility.java
+++ src/test/java/com/utility/BrowserUtility.java
@@ -114,7 +114,7 @@ public abstract class BrowserUtility {
 		Date date = new Date();
 		SimpleDateFormat format = new SimpleDateFormat("HH-mm-ss");
 		String timeStamp = format.format(date);
-		String path = System.getProperty("user.dir") + "/screenshots/" + name + "_" + timeStamp + ".png";
+		String path ="./screenshots/" + name + "_" + timeStamp + ".png";
 
 		File screenShotFile = new File(path);
 		try {
